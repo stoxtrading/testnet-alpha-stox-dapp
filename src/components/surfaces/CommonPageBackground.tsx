8d071ebf1b9fda4283bf43cbbc606140c83e7229
Philippe ARON <philippe.aron@arlequinfinance.com>
2025-02-17 14:25:16 +0100

    Refactor CommonPageBackground component to use inline styles for centering and background image

--- src/components/surfaces/CommonPageBackground.tsx
+++ src/components/surfaces/CommonPageBackground.tsx
@@ -1,6 +1,5 @@
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import './CommonPageBackground.css';
 import CssBaseline from "@mui/material/CssBaseline";
 
 interface CommonPageBackgroundProps {
@@ -11,7 +10,15 @@ interface CommonPageBackgroundProps {
 const CommonPageBackground: React.FC<CommonPageBackgroundProps> = ({ children }) => (
 
 
-    <div className='pageSurroundingArea'>
+    <div className='pageSurroundingArea'
+        style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            backgroundImage: 'url(./upscale-1.jpg)',
+        }}
+    >
         <CssBaseline />
 
         <Container >
