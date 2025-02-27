59c2944b0aa020094f31db3669a6a5146eafa7e6
Philippe ARON <philippe.aron@arlequinfinance.com>
2025-02-27 15:32:18 +0100

    Refactor RealTimePrice and Trading components to improve price tracking and color indication; utilize useRef for previous price reference and enhance layout properties for better visual feedback

--- src/components/realTimePrice/RealTimePrice.tsx
+++ src/components/realTimePrice/RealTimePrice.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid2';
 import Stack from '@mui/material/Stack';
@@ -6,9 +6,12 @@ import { GenericTypography, NumbersTypography, } from '../../assets/elements/Cus
 
 
 const RealTimePrice = () => {
-    const [price, setPrice] = useState<number | null>(null);
+    const [price, setPrice] = useState<number>(0);
     const [priceChange, setPriceChange] = useState<number | null>(null);
     const [priceChangeAbs, setPriceChangeAbs] = useState<number | null>(null);
+
+    const prevPriceRef = useRef<number>(10);
+
 
 
     useEffect(() => {
@@ -23,9 +26,12 @@ const RealTimePrice = () => {
         ws.onmessage = (event) => {
             const data = JSON.parse(event.data);
             if (data.body && data.body.NVDA_cboe && data.body.NVDA_cboe.last) {
+                //prevPriceRef.current = price;
                 setPrice(data.body.NVDA_cboe.last);
                 setPriceChange(data.body.NVDA_cboe['24hPercChg']);
                 setPriceChangeAbs(data.body.NVDA_cboe['24hPxChg']);
+
+
             }
         };
 
@@ -42,8 +48,15 @@ const RealTimePrice = () => {
         };
     }, []);
 
+    useEffect(() => {
+		prevPriceRef.current = price;
+	}, [price]);
 
 
+    const getColor = () => {
+        if (prevPriceRef.current === null || price === null) return 'white';
+        return price > prevPriceRef.current ? '#27AE60' : price < prevPriceRef.current ? 'red' : '#27AE60';
+    };
 
     return (
         <Box >
@@ -51,7 +64,7 @@ const RealTimePrice = () => {
 
                 <Grid container alignItems="top" justifyContent="left" >
                     <Grid size={{ xs: 1, sm: 1 }} sx={{
-                      
+
                         display: 'flex',
                         justifyContent: 'center',
                         alignItems: 'center'
@@ -59,7 +72,7 @@ const RealTimePrice = () => {
                         <img src='./nvda.png' width="30" height="30" />
                     </Grid>
                     <Grid size={{ xs: 7, sm: 4 }} alignContent="center" justifyContent="center" sx={{
-                       
+
                         display: 'flex',
                         justifyContent: 'center',
                         alignItems: 'center'
@@ -71,26 +84,27 @@ const RealTimePrice = () => {
                         </GenericTypography>
                     </Grid>
                     <Grid size={{ xs: 3, sm: 2 }} sx={{
-                       
-                       display: 'flex',
-                       justifyContent: 'center',
-                       alignItems: 'center',
-                       alignnContent: 'center'
-                   }} >
-                        <NumbersTypography fontSize={"1em"} paddingTop='0.4rem'>
+
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        alignnContent: 'center'
+                    }} >
+                        <NumbersTypography fontSize={"1.2em"} paddingTop='0.4rem' color={getColor()}>
                             {price !== null ? `$${price.toFixed(2)}` : 'Loading...'}
                         </NumbersTypography>
                     </Grid>
-                    <Grid display={{ xs: 'none', sm: 'flex' }} size={{ xs: 4, sm: 3 }} paddingTop='0.4rem' sx={{
+                    <Grid display={{ xs: 'none', sm: 'flex' }} size={{ xs: 4, sm: 4 }} paddingTop='0.4rem' sx={{
                         justifyContent: 'center',
-                       alignItems: 'center',
-                       alignnContent: 'center'}}
-                       >
+                        alignItems: 'center',
+                        alignnContent: 'center'
+                    }}
+                    >
                         <NumbersTypography fontSize={"0.8em"} >
-                            {priceChange !== null ? `${(priceChange * 100).toFixed(2)}%` : 'Loading...'}&nbsp;{priceChangeAbs !== null ? `(${(priceChangeAbs).toFixed(2)})` : 'Loading...'}
+                            {priceChange !== null ? `${(priceChange * 100).toFixed(2)}%` : 'Loading...'}&nbsp;{priceChangeAbs !== null ? `(${(priceChangeAbs).toFixed(2)})/24h` : 'Loading...'}
                         </NumbersTypography>
                     </Grid>
-                   
+
                 </Grid>
             </Stack>
         </Box>
